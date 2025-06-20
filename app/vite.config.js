2769a5dfbdae966e76d575c086994e89b59ebb81
Benjamin Churchill <bjc9001@gmail.com>
2025-06-20 08:02:40 -0500

    add gh actions etc

--- app/vite.config.js
+++ app/vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/', // Set to '/' if using a custom domain (CNAME present), otherwise '/qWeX23.github.io/'
   plugins: [react()],
 })
