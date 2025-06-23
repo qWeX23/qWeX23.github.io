0e1b9ff849429b468c2802f34342c76d55350fbb
Benjamin Churchill <bjc9001@gmail.com>
2025-06-23 08:14:44 -0500

    Update intro and projects pages

--- app/src/pages/Intro.jsx
+++ app/src/pages/Intro.jsx
@@ -1,8 +1,11 @@
 export default function Intro() {
   return (
-    <div>
-      <h1>Welcome</h1>
-      <p>This is the intro page.</p>
-    </div>
+    <section>
+      <h1>I am Benjamin Churchill</h1>
+      <p>
+        I am a software engineer with 10 years experience specializing in backend
+        and data engineering.
+      </p>
+    </section>
   );
 }
